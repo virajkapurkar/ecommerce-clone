53af8f608e16f7c5e79347312c8a7a8f65d4d4d4
Viraj Kapurkar <viraj.k.kapurkar@oracle.com>
2022-08-30 10:48:14 +0530

    Added post route for new user registration

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -35,4 +35,28 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authController, getUserProfile };
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  //check if user is already in the database
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    res.status(400);
+    throw new Error("User Already exists!");
+  }
+
+  //if user is not in the database - create new
+  const newUser = await User.create({ name, email, password });
+  if (newUser) {
+    res.status(200).json({
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+      isAdmin: newUser.isAdmin,
+      token: generateToken(newUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("Could not create user :(");
+  }
+});
+export { authController, getUserProfile, registerUser };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,9 +3,15 @@ const router = express.Router();
 import {
   authController,
   getUserProfile,
+  registerUser,
 } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
+//Route : POST register new user
+router.route("/").post(registerUser);
+//Route : POST login request
 router.post("/login", authController);
+//Protected Route : GET User profile
 router.route("/profile").get(protect, getUserProfile);
+
 export default router;
