cede4bed556e042c9934429ec0eeb2f7e18109c5
Viraj Kapurkar <viraj.k.kapurkar@oracle.com>
2022-09-01 11:06:33 +0530

    Added logout functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -36,3 +36,10 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const logout = () => async (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({
+    type: USER_LOGOUT,
+  });
+};
--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -9,6 +9,13 @@ import ShoppingCartSharpIcon from "@mui/icons-material/ShoppingCartSharp";
 import PersonIcon from "@mui/icons-material/Person";
 import { createTheme, ThemeProvider } from "@mui/material";
 import { NavLink } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Logout from "@mui/icons-material/Logout";
+import Link from "@mui/material/Link";
+import { logout } from "../actions/userActions.js";
 
 const theme = createTheme();
 theme.typography.body2 = {
@@ -21,6 +28,19 @@ const linkStyle = {
 };
 
 function Navbar(props) {
+  const dispatch = useDispatch();
+  const { userInfo } = useSelector((state) => state.userLogin);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const open = Boolean(anchorEl);
+  const handleLogout = () => {
+    dispatch(logout());
+  };
   return (
     <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
@@ -43,15 +63,77 @@ function Navbar(props) {
                 </Button>
               </NavLink>
 
-              <NavLink to="/login" style={linkStyle}>
-                <Button color="inherit">
-                  {" "}
-                  <PersonIcon /> &nbsp;{" "}
-                  <Typography variant="body2" display="block">
-                    Login
-                  </Typography>
-                </Button>
-              </NavLink>
+              {userInfo ? (
+                <>
+                  <Button color="inherit" onClick={handleClick}>
+                    {" "}
+                    <PersonIcon /> &nbsp;{" "}
+                    <Typography variant="body2" display="block">
+                      {userInfo.name}
+                    </Typography>
+                  </Button>
+                  <Menu
+                    anchorEl={anchorEl}
+                    id="user-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                      elevation: 0,
+                      sx: {
+                        overflow: "visible",
+                        filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                        mt: 1.5,
+                        "& .MuiAvatar-root": {
+                          width: 32,
+                          height: 32,
+                          ml: -0.5,
+                          mr: 1,
+                        },
+                        "&:before": {
+                          content: '""',
+                          display: "block",
+                          position: "absolute",
+                          top: 0,
+                          right: 14,
+                          width: 10,
+                          height: 10,
+                          bgcolor: "background.paper",
+                          transform: "translateY(-50%) rotate(45deg)",
+                          zIndex: 0,
+                        },
+                      },
+                    }}
+                    transformOrigin={{ horizontal: "right", vertical: "top" }}
+                    anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+                  >
+                    <Link
+                      href="/profile"
+                      sx={{ textDecoration: "none", color: "black" }}
+                    >
+                      <MenuItem>
+                        <PersonIcon /> &nbsp; &nbsp;Profile
+                      </MenuItem>
+                    </Link>
+                    <MenuItem onClick={handleLogout}>
+                      <ListItemIcon>
+                        <Logout fontSize="small" />
+                      </ListItemIcon>
+                      Logout
+                    </MenuItem>
+                  </Menu>
+                </>
+              ) : (
+                <NavLink to="/login" style={linkStyle}>
+                  <Button color="inherit">
+                    {" "}
+                    <PersonIcon /> &nbsp;{" "}
+                    <Typography variant="body2" display="block">
+                      Login
+                    </Typography>
+                  </Button>
+                </NavLink>
+              )}
             </Toolbar>
           </Container>
         </AppBar>
